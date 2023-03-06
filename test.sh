c376ba8d66fda48c2d5bb0ffc4104f46a5e688aa
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-03-05 20:35:11 -0800

    added test.sh

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+clang -O3 -o test_xseed.out test_xseed.c xdrop_aligner.c ntlookup.c fasta_map.a && diff -s ground_truth_seriasm/seeds.sorted.after.paf <(./test_xseed.out | sort -h -k1,1 -k6,6)
+
