da0a4616b3ac82e406717c6b4eb14b83320bf4e1
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-03-05 17:06:56 -0800

    ground truth seeds are consistent with my code

--- test_xseed.c
+++ test_xseed.c
@@ -2,24 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "fasta_map.h"
 #include "xdrop_aligner.h"
 #include "usage.h"
+
+char const *fasta_fname = "ground_truth_seriasm/reads.fa";
+char const *seeds_fname = "ground_truth_seriasm/seeds.sorted.before.paf";
 
 int numreads;
 char *buf, **seqs;
 
 int main(int argc, char *argv[])
 {
-    fasta_map_t fmap = fasta_map_create("ground_truth_seriasm/reads.fa");
+    fasta_map_t fmap = fasta_map_create(fasta_fname);
     numreads = loadseqs(fmap, &buf, &seqs);
     fasta_map_free(fmap);
 
-    for (int i = 0; i < numreads; ++i)
+    FILE *f = fopen(seeds_fname, "r");
+
+    int idxQ, lenQ, begQ, endQ, idxT, lenT, begT, endT;
+
+    while (fscanf(f, "%d %d %d %d %*c %d %d %d %d %*d %*d %*d %*s\n", &idxQ, &lenQ, &begQ, &endQ, &idxT, &lenT, &begT, &endT) > 0)
     {
-        printf("%s\n", seqs[i]);
+        char *seqQ = seqs[idxQ-1];
+        char *seqT = seqs[idxT-1];
+        int lenQ_chk = strlen(seqQ);
+        int lenT_chk = strlen(seqT);
+
+        assert(lenQ_chk == lenQ);
+        assert(lenT_chk == lenT);
     }
 
+    fclose(f);
     free(buf);
     free(seqs);
 
